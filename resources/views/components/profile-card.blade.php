db84284e89b749ffd5966aaf19e8af34d6ff7720
Jaz-ma <potatoorgy3@gmail.com>
2022-07-21 14:36:09 +0100

    form

--- resources/views/components/profile-card.blade.php
+++ resources/views/components/profile-card.blade.php
@@ -1,22 +1,20 @@
 <div x-data="{text_open: true}" class="flex  min-h-fit justify-around items-center ">
         <div class="flex">
-           <div class="flex flex-col justify-between  text-xl mx-4">
-                <div class="py-4">
+           <div x-show="text_open"class="flex flex-col justify-between  text-xl mx-4">
+                <div class="py-4 ">
                 {{ __('Username')}}
                 </div>
-                <div class="py-4">
+                <div class="py-4 ">
                 {{ __('Name')}}
                 </div>
-                <div class="py-4">
+                <div class="py-4 ">
                 {{ __('Email')}}
                 </div>
 
                 <div x-show="!text_open" class="py-4 ">
                 {{ __('Password')}}
                 </div>
-                 {{-- <div x-show="!text_open" class="py-4 ">
-                {{ __('Confirmation')}}
-                </div> --}}
+
             </div>
 
             <div x-show="text_open" class="flex flex-col justify-between text-lg text-blue-600 mx-4">
@@ -30,38 +28,51 @@
                 {{ Auth::user()->email}}
                 </div>
             </div>
-            <form action="{{route('user.update',Auth::user()->id)}}" method="POST">
+
+            <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+            <form  id="edit_profile_form"action="{{route('user.update',Auth::user()->id)}}" method="POST" class="flex flex-col justify-center">
                     @csrf
                     @method('PUT')
             <div x-show="!text_open" class="flex flex-col justify-between text-lg text-blue-600 mx-4">
                 <div class="py-4">
+
+                    <x-label for="username" :value="__('Username')" />
+
                     <x-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus />
 
                 </div>
                 <div class="py-4">
 
+                    <x-label for="name" :value="__('Name')" />
+
                     <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
                 </div>
                 <div class="py-4">
+
+                    <x-label for="email" :value="__('Email')" />
 
                     <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
                 </div>
                 <div class="py-4">
 
+                    <x-label for="password" :value="__('Confirm your password')" />
+
                     <x-input id="password" class="block mt-1 w-full" type="password" name="password" :value="old('password')" required autofocus />
                 </div>
-                <x-button class="mt-4 ml-4" x-show="!text_open">
-                        Save Changes
-                    </x-button>
             </div>
+                 <x-button
+                         @click="text_open = false"
+                         x-text="text_open? 'Edit' : 'Save'"
+                         class="ml-4"
+                         >
 
+                </x-button>
                 </form>
 
         </div>
         <div >
-                <x-button @click.prevent="text_open = !text_open">
-                    Edit
-                </x-button>
+
 
         </div>
 </div>
